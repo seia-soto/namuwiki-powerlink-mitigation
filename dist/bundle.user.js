3520bdabfb0d04146c03bd1046bf0bb245811bfa
HoJeong Go <seia@outlook.kr>
2022-06-23 03:31:30 +0900

    chore: update metadata

--- dist/bundle.user.js
+++ dist/bundle.user.js
@@ -2,7 +2,7 @@
 // @encoding    utf-8
 // @name        namuwiki-powerlink-mitigation
 // @description Mitigate powerlink ads from NamuWiki.
-// @version     1.3.0
+// @version     1.3.1
 // @author      HoJeong Go <seia@outlook.kr>
 // @license     ISC
 //
@@ -19,4 +19,4 @@
 // @run-at      document-start
 // ==/UserScript==
 
-(()=>{var n=unsafeWindow||window,i={secret:Math.random()+(Date.now()*Math.random()).toString(36)},y=new Proxy(console.log,{apply(o,t,e){Reflect.apply(o,t,["[namuwiki-powerlink-mitigation]",...e])}});n.open=new Proxy(n.open,{apply(o,t,e){let[l]=e,r=()=>Reflect.apply(o,t,e);return e.indexOf(i.secret)>=0?l:r()}});n.HTMLTableElement.prototype.appendChild=new Proxy(n.HTMLTableElement.prototype.appendChild,{apply(o,t,e){let[l]=e,r=()=>Reflect.apply(o,t,e);if(!l.querySelectorAll("img").length)return r();let c=l.querySelectorAll("a");for(let d of c){let f=n.open,s=!1;if(n.open=new Proxy(n.open,{apply(m,u,a){a.push(i.secret);let p=Reflect.apply(m,u,a);return typeof p=="string"&&(p.includes("saedu.naver.com/adbiz/searchad/")||p.includes("adcr.naver.com/adcr?x="))&&(s=!0),p}}),d.click(),n.open=f,s)return setTimeout(()=>{y("removing the target element"),t.remove()},1),l}}});})();
+(()=>{var n=unsafeWindow||window,r={secret:Math.random()+(Date.now()*Math.random()).toString(36)},y=new Proxy(console.log,{apply(l,t,e){Reflect.apply(l,t,["[namuwiki-powerlink-mitigation]",...e])}});n.open=new Proxy(n.open,{apply(l,t,e){let[o]=e,s=()=>Reflect.apply(l,t,e);return e.indexOf(r.secret)>=0?o:s()}});n.HTMLTableElement.prototype.appendChild=new Proxy(n.HTMLTableElement.prototype.appendChild,{apply(l,t,e){let[o]=e,s=()=>Reflect.apply(l,t,e);if(!(o instanceof HTMLElement)||!o.querySelectorAll("img").length)return s();let c=o.querySelectorAll("a");for(let d of c){let f=n.open,a=!1;if(n.open=new Proxy(n.open,{apply(m,u,i){i.push(r.secret);let p=Reflect.apply(m,u,i);return typeof p=="string"&&(p.includes("saedu.naver.com/adbiz/searchad/")||p.includes("adcr.naver.com/adcr?x="))&&(a=!0),p}}),d.click(),n.open=f,a)return setTimeout(()=>{y("removing the target element"),t.remove()},1),o}}});})();
