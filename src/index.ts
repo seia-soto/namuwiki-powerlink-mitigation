7dec9e59ed1b5be373fb2c29eb4d7b9ee9b6f858
HoJeong Go <seia@outlook.kr>
2022-06-23 03:16:08 +0900

    feat: mitigate by removing the target element

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,8 @@
 // eslint-disable-next-line no-undef
 const frame: typeof window = unsafeWindow || window;
 
-const key = 'INITIAL_STATE';
 const state = {
-	[key]: [] as unknown[],
-	fired: false,
-	warningShown: false,
+	secret: Math.random() + (Date.now() * Math.random()).toString(36),
 };
 
 const log = new Proxy(
@@ -18,88 +15,76 @@ const log = new Proxy(
 	},
 );
 
-const noop = (a?: any) => a;
-
-const appendWarning = () => {
-	if (state.warningShown) {
-		return;
-	}
-
-	const ruid = (Date.now() * Math.random() * Math.atanh(Date.now())).toString(36).slice(0, 6);
-
-	frame.document.querySelector('div#app>div[id]>div+div')?.insertAdjacentHTML(
-		'beforebegin',
-		`
-<div id="${ruid}">
-  <style>
-    #${ruid} { padding: 2px 16px; }
-  </style>
-  <p>
-    <strong>namuwiki-powerlink-mitigation</strong>
-    광고 값이 정상적으로 차단되지 않았을 수 있습니다.
-    아래에서 속성 값을 복사하여
-    <a
-      href="https://github.com/seia-soto/namuwiki-powerlink-mitigation/issues"
-    >여기</a>에서 제보해주세요.
-    <br/>
-
-    <button>속성 값 보기</button>
-  </p>
-</div>
-`,
-	);
-
-	const inserted = frame.document.getElementById(ruid);
-
-	inserted
-		?.querySelector('button')
-		?.addEventListener('click', () => {
-			const content = JSON.stringify({
-				[key]: state[key],
-				ua: navigator.userAgent,
-			})
-				.replace(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/g, 'IP_ADDR_HIDDEN');
-
-			frame.prompt('Please copy to clipboard', content);
-		}, {once: true});
-
-	state.warningShown = true;
-};
-
-const defusePowerlink = (root: object) => {
-	for (const property in root) {
-		if (typeof root[property] === 'object') {
-			root[property] = defusePowerlink(root[property]);
-		} else if (typeof root[property] === 'string' && (root[property] as string).indexOf('adcr') >= 0) {
-			try {
-				JSON.parse(root[property]);
-
-				log('defusing ad alignments');
-
-				state.fired = true;
-
-				return '[]';
-			} catch (error) {
-				noop(error);
-			}
-		}
-	}
-
-	return root;
-};
-
-Object.defineProperty(
-	frame,
-	key,
+frame.open = new Proxy(
+	frame.open,
 	{
-		get() {
-			return state[key][state[key].length - 1];
-		},
-		set(v) {
-			state[key].push(defusePowerlink(v));
+		apply(target, thisArg, argArray) {
+			const [href] = argArray as string[];
 
-			if (!state.fired) {
-				appendWarning();
+			const bypassHandler = () => Reflect.apply(target, thisArg, argArray);
+
+			if (argArray.indexOf(state.secret) >= 0) {
+				return href;
+			}
+
+			return bypassHandler();
+		},
+	},
+);
+
+frame.HTMLTableElement.prototype.appendChild = new Proxy(
+	frame.HTMLTableElement.prototype.appendChild,
+	{
+		apply(target, thisArg: HTMLTableElement, argArray) {
+			const [child] = argArray as HTMLElement[];
+			const bypassHandler = () => Reflect.apply(target, thisArg, argArray);
+
+			if (!child.querySelectorAll('img').length) {
+				return bypassHandler();
+			}
+
+			const innerLinks = child.querySelectorAll('a');
+
+			for (const link of innerLinks) {
+				const original = frame.open;
+				let isPowerlinkAd = false;
+
+				frame.open = new Proxy(
+					frame.open,
+					{
+						apply(target, thisArg, argArray) {
+							argArray.push(state.secret);
+
+							const result = Reflect.apply(target, thisArg, argArray);
+
+							if (
+								typeof result === 'string'
+								&& (
+									result.includes('saedu.naver.com/adbiz/searchad/')
+									|| result.includes('adcr.naver.com/adcr?x=')
+								)
+							) {
+								isPowerlinkAd = true;
+							}
+
+							return result;
+						},
+					},
+				);
+
+				link.click();
+
+				frame.open = original;
+
+				if (isPowerlinkAd) {
+					setTimeout(() => {
+						log('removing the target element');
+
+						thisArg.remove();
+					}, 1);
+
+					return child;
+				}
 			}
 		},
 	},
